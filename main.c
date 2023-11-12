93697b463611b47b51a148a386624a1035525eee
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-11-12 18:15:43 +0000

    second

--- main.c
+++ main.c
@@ -9,18 +9,15 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARGS 64
 
-extern char **environ; // External variable containing the environment
+extern char **environ;
 
 char *custom_getline() {
     static char buffer[BUFFER_SIZE];
     static size_t buffer_index = 0;
     static ssize_t bytes_read = 0;
 
-    // If the buffer is empty, read more data
     if (buffer_index >= bytes_read || bytes_read == 0) {
         bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
-
-        // If read returns 0, we have reached the end of the input
         if (bytes_read <= 0) {
             return NULL;
         }
@@ -28,25 +25,20 @@ char *custom_getline() {
         buffer_index = 0;
     }
 
-    // Find the end of the line or end of the buffer
     size_t i = buffer_index;
     while (i < bytes_read && buffer[i] != '\n') {
         i++;
     }
 
-    // Allocate memory for the line
     size_t line_length = i - buffer_index;
     char *line = (char*)malloc(line_length + 1);
 
-    // Copy the line from the buffer to the allocated memory
     for (size_t j = 0; j < line_length; j++) {
         line[j] = buffer[buffer_index + j];
     }
 
-    // Null-terminate the line
     line[line_length] = '\0';
 
-    // Move the buffer_index to the next character after the newline
     buffer_index = i + 1;
 
     return line;
@@ -57,16 +49,26 @@ void display_prompt() {
     fflush(stdout);
 }
 
-int parse_input(char *input, char *args[]) {
+int parse_input(char *input, char *commands[]) {
     int i = 0;
-    char *token = strtok(input, " ");
+    char *token = strtok(input, ";");
 
     while (token != NULL && i < MAX_ARGS - 1) {
-        args[i++] = token;
-        token = strtok(NULL, " ");
+        while (*token == ' ' || *token == '\t') {
+            token++;
+        }
+
+        char *end = token + strlen(token) - 1;
+        while (end > token && (*end == ' ' || *end == '\t')) {
+            end--;
+        }
+        end[1] = '\0';
+
+        commands[i++] = token;
+        token = strtok(NULL, ";");
     }
 
-    args[i] = NULL; // Set the last element to NULL for execvp
+    commands[i] = NULL;
     return i;
 }
 
@@ -91,9 +93,8 @@ int custom_setenv(const char *name, const char *value, int overwrite) {
                 fprintf(stderr, "setenv: Variable '%s' already exists\n", name);
                 return -1;
             } else {
-                // Update the existing variable
                 free(*env);
-                *env = malloc(strlen(name) + strlen(value) + 2); // +1 for '=' and +1 for null terminator
+                *env = malloc(strlen(name) + strlen(value) + 2);
                 sprintf(*env, "%s=%s", name, value);
                 return 0;
             }
@@ -101,7 +102,6 @@ int custom_setenv(const char *name, const char *value, int overwrite) {
         env++;
     }
 
-    // Variable doesn't exist, create a new one
     char **new_env = malloc((env - environ + 2) * sizeof(char *));
     memcpy(new_env, environ, (env - environ) * sizeof(char *));
     new_env[env - environ] = malloc(strlen(name) + strlen(value) + 2);
@@ -125,7 +125,6 @@ int custom_unsetenv(const char *name) {
 
     while (*env != NULL) {
         if (strncmp(*env, name, strlen(name)) == 0 && (*env)[strlen(name)] == '=') {
-            // Skip the variable to remove it
             env++;
             continue;
         }
@@ -141,13 +140,11 @@ int custom_unsetenv(const char *name) {
 }
 
 int custom_cd(const char *dir) {
-    const char *new_dir; // Declare new_dir as const pointer
+    const char *new_dir;
 
     if (dir == NULL || strcmp(dir, "") == 0) {
-        // If no argument is given, change to the home directory
         new_dir = getenv("HOME");
     } else if (strcmp(dir, "-") == 0) {
-        // If the argument is "-", change to the previous directory
         new_dir = getenv("OLDPWD");
         if (new_dir == NULL) {
             fprintf(stderr, "cd: OLDPWD not set\n");
@@ -156,27 +153,22 @@ int custom_cd(const char *dir) {
     } else {
         new_dir = dir;
     }
-
-    // Get the current working directory for updating PWD and OLDPWD
     char current_dir[BUFFER_SIZE];
     if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
         perror("getcwd");
         return -1;
     }
 
-    // Update OLDPWD
     if (setenv("OLDPWD", current_dir, 1) != 0) {
         perror("setenv");
         return -1;
     }
 
-    // Change to the new directory
     if (chdir(new_dir) != 0) {
         perror("chdir");
         return -1;
     }
 
-    // Update PWD
     if (setenv("PWD", new_dir, 1) != 0) {
         perror("setenv");
         return -1;
@@ -185,96 +177,102 @@ int custom_cd(const char *dir) {
     return 0;
 }
 
-int main() {
-    char input[MAX_INPUT_SIZE];
+int execute_command(char *command) {
     char *args[MAX_ARGS];
+    int arg_count = parse_input(command, args);
 
-    while (1) {
-        // Display prompt and read user input
-        display_prompt();
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            // Handle end of file (Ctrl+D)
-            printf("\n");
-            break;
+    int success = 1;
+
+    for (int i = 0; i < arg_count; i++) {
+        char *arg = args[i];
+
+        if (strcmp(arg, "&&") == 0) {
+            if (success) {
+                continue;
+            } else {
+                break;
+            }
+        } else if (strcmp(arg, "||") == 0) {
+            if (!success) {
+                continue;
+            } else {
+                break;
+            }
         }
 
-        // Remove the newline character at the end of input
-        input[strcspn(input, "\n")] = '\0';
-
-        // Check for the built-in commands
-        if (strncmp(input, "exit", 4) == 0) {
-            // Parse the exit command and extract the status code
+        if (strncmp(arg, "exit", 4) == 0) {
             int status_code = 0;
-            if (strlen(input) > 4) {
-                status_code = atoi(input + 5);
+            if (strlen(arg) > 4) {
+                status_code = atoi(arg + 5);
             }
 
             printf("Exiting shell with status code: %d\n", status_code);
             exit(status_code);
-        } else if (strcmp(input, "env") == 0) {
+        } else if (strcmp(arg, "env") == 0) {
             print_environment();
-            continue; // Skip the fork and wait steps for built-in commands
-        } else if (strncmp(input, "setenv", 6) == 0) {
-            // Parse the setenv command and call the custom_setenv function
-            char *name = strtok(input + 6, " ");
+        } else if (strncmp(arg, "setenv", 6) == 0) {
+            char *name = strtok(arg + 6, " ");
             char *value = strtok(NULL, " ");
             if (name != NULL && value != NULL) {
                 custom_setenv(name, value, 1);
             } else {
                 fprintf(stderr, "setenv: Invalid syntax\n");
             }
-            continue; // Skip the fork and wait steps for built-in commands
-        } else if (strncmp(input, "unsetenv", 8) == 0) {
-            // Parse the unsetenv command and call the custom_unsetenv function
-            char *name = strtok(input + 8, " ");
+        } else if (strncmp(arg, "unsetenv", 8) == 0) {
+            char *name = strtok(arg + 8, " ");
             if (name != NULL) {
                 custom_unsetenv(name);
             } else {
                 fprintf(stderr, "unsetenv: Invalid syntax\n");
             }
-            continue; // Skip the fork and wait steps for built-in commands
-        } else if (strncmp(input, "cd", 2) == 0) {
-            // Parse the cd command and call the custom_cd function
-            char *dir = strtok(input + 2, " ");
+        } else if (strncmp(arg, "cd", 2) == 0) {
+            char *dir = strtok(arg + 2, " ");
             if (dir != NULL) {
                 custom_cd(dir);
             } else {
                 custom_cd(NULL);
             }
-            continue; // Skip the fork and wait steps for built-in commands
-        }
-
-        // Parse input to get command and arguments
-        int arg_count = parse_input(input, args);
-
-        // Fork a new process only if the command exists in PATH
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            // Error in forking
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            // Execute the command using execvp
-            if (execvp(args[0], args) == -1) {
-                // Handle command not found
-                perror("execvp");
-                exit(EXIT_FAILURE);
-            }
         } else {
-            // Parent process
-            // Wait for the child process to complete
-            int status;
-            waitpid(pid, &status, 0);
+            pid_t pid = fork();
 
-            // Check if the child process exited successfully
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-                printf("Error: Command not found\n");
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pid == 0) {
+                if (execvp(arg, args) == -1) {
+                    perror("execvp");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                int status;
+                waitpid(pid, &status, 0);
+
+                if (WIFEXITED(status)) {
+                    success = (WEXITSTATUS(status) == 0);
+                } else {
+                    success = 0;
+                }
             }
         }
     }
 
     return 0;
 }
+          
 
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        display_prompt();
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+        execute_command(input);
+    }
+
+    return 0;
+}
